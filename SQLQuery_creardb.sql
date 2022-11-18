c9b04ff83bd4ee49233c35b63ca3ba4f07bfc967
Darkpianist88 <davidmoref@gmail.com>
2022-11-18 12:45:08 +0100

    David

--- SQLQuery_creardb.sql
+++ SQLQuery_creardb.sql
@@ -8,3 +8,22 @@ GO
 USE DB_GRUPO_0;
 GO
 
+CREATE TABLE Facturas 
+(
+ID_Factura INT IDENTITY (1,1) PRIMARY KEY, 
+ID_Cliente int not null, 
+Facturado date,
+Detalle char (40), 
+ID_Siniestro int not null
+)
+
+CREATE TABLE Detalles
+(
+ID_Factura int,  
+Concepto varchar,
+Total int
+)
+GO
+
+
+
