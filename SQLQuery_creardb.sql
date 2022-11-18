df4e9d3e5f506b28e99f619c6e536eae6eda8305
Mar <CEFEM301@outlook.es>
2022-11-18 11:11:32 +0100

    Creación DB Seguros

---
+++ SQLQuery_creardb.sql
@@ -0,0 +1,10 @@
+---------------------------------------------------------------------
+-- Script that creates the sample database DB_GRUPO_0
+--
+-- Create database
+CREATE DATABASE DB_GRUPO_0;
+GO
+
+USE DB_GRUPO_0;
+GO
+
