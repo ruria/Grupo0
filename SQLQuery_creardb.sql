28b560843febdca3750cbc5ed79566c737575c91
Lydiaddc <pepitopiscinas1975@gmail.com>
2022-11-18 13:02:11 +0100

    Lydia

--- SQLQuery_creardb.sql
+++ SQLQuery_creardb.sql
@@ -8,3 +8,21 @@ GO
 USE DB_GRUPO_0;
 GO
 
+Create table Clientes
+(
+ID_Cliente int Identity (1,1) primary key not null,
+NIF char (20),
+[Nombre y apellidos] varchar (64),
+Direccion varchar (max),
+Telefono char (20),
+[E-mail] varchar
+)
+
+Create table Empleados
+(
+ID_Empleado int Identity (1,1) primary key not null,
+Nombre varchar,
+Apellidos char (30),
+Telefono char (20)
+)
+
